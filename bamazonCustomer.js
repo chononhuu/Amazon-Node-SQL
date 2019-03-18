49e411d3171e256ad805b9b107fb291b18660fba
chononhuu <bts206@hotmail.com>
2019-03-18 00:29:06 -0600

    updated line of code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Zail2sda",
+    password: "password",
     database: "bamazon"
 });
 
